0b922721c27f11770e990325ef0d9071959a6268
bellashook12 <bellakshook@gmail.com>
2024-03-08 17:12:23 -0500

    3/8

--- src/banner-app.js
+++ src/banner-app.js
@@ -8,42 +8,65 @@ export class BannerApp extends LitElement {
   
   constructor() {
     super();
+    this.sticky = false;
+    this.status = "notice";
+    this.date = "NOVEMBER 17, 2023 12:00 AM";
+    this.opened = true;
+    this.message = "";
+    
     
   }
 
   static get styles() {
     return css`
       :host {
-        display: block;
+        display: flex;
+        --display-mode: unset;
+        --display-mode-opposite: none;
+        --min-banner-height: 20vh;
+        min-height: var(--min-banner-height);
+        
+
       }
 
-       .wrapper{
-        min-height: 100vh;
-        
+       :host([sticky]) {
+        position: sticky;
+        top: 0;
+        z-index: 9999;
       } 
 
-      .banner{
+      :host([status = "notice"]) {
+        --background-color: blue;
+        
+      }
 
+      :host([status = "warning"]) {
+        --background-color: #bf8226;
+        
+      }
+
+      :host([status ="alert"]) {
+        --background-color:#c0250d;
+        
+      }
+
+
+      .maincontainer{
         width: 100%;
-        height: 20vh;
-        position: sticky;
+        height: 150px;
         top: 0px;
-        background-color: blue;
-      }
-      
-      p{
-        color:white;
-      }
-      
-      button{
-        background-color: transparent;
-        border: 0px;
+        background-color: var(--background-color);
+        border: 8px;
+        margin: 8px;
       }
 
-      button:hover {
-      border: dashed;
-    } 
+      .date {
+        color: white;
+      }
 
+     
+
+      
       
        
   `;
@@ -52,50 +75,82 @@ export class BannerApp extends LitElement {
   
   render() {
     return html`
-    <div class="wrapper">
-        <div class="banner">
-        <p> trial</p>
-        <button class="expand-alert">
-        <svg xmlns="http://www.w3.org/2000/svg" width="82" height="82" viewBox="0 0 82 82" class="alert-icon">
-          <g transform="translate(-350.099 -428.714)">
-            <g transform="translate(350.099 428.714)" fill="none" stroke-width="6">
-              <circle cx="41" cy="41" r="41" stroke="none"></circle>
-              <circle cx="41" cy="41" r="38" fill="none"></circle>
-            </g>
-            <g transform="translate(384.41 448.566)">
-              <rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect>
-              <path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)" class="alert-icon-min"></path>
-            </g>
-          </g>
-        </svg>
 
-        <div class="minimize-alert">
-        <button>
-          <span> X Close   
+    <div class= "maincontainer">
+      <div class="opencontainer">
+        <span class="openit">
+          <button id="openthis" @click="${this.openAll}"> TEST CAMPUS ALERT </button>
+          </span>
+          </div>
+
+          <div class = "date">
+            ${this.date}
+          </div>
+
+    
+          <svg height ="32px" width ="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84 84" class="alert-icon">
+            <g transform="translate(-350.099 -428.714)">
+              <g transform="translate(350.099 428.714)" fill="none" stroke-width="6">
+                <circle cx="41" cy="41" r="41" stroke="black"></circle>
+                <circle cx="41" cy="41" r="38" fill="none"></circle>
+              </g>
+              <g transform="translate(384.41 448.566)">
+                <rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect>
+                <path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)"></path>
+              </g>
+            </g>
+          </svg>
+        
+          <div class = "messageInfo" >
+          ${this.message}
+          </div>
+
+          <span class= "closeit">
+            <button id="closethis" @click="${this.closeAll}">✕ Close</button>
   </span>
-  </button>
-  </div>
 
-  
-  
-    <slot></slot>
+
+
+
+
     </div>
-    </div>
-   
+    
     
     `;
 
+  }
+
+  openAll(){
+    this.opened= true;
+    console.log("opened");
+    this.style.setProperty('--display-mode', 'unset');
+    this.style.setProperty('--display-mode-opposite', 'none');
+    this.style.setProperty('--min-banner-height', '20vh');
+  }
+
+  closeAll(){
+    this.opened= false;
+    console.log("closed");
+    this.style.setProperty('--display-mode', 'none');
+    this.style.setProperty('--display-mode-opposite', 'none');
+    this.style.setProperty('--min-banner-height', '6vh');
   }
   
   
 
   static get properties() {
     return {
-      counter: { type: Number, reflect: true },
+      opened: { type: Boolean, reflect: true },
+      status: { type: String, reflect: true },
+      sticky: { type: Boolean, reflect: true },
+      date: { type: String, reflect: true },
+      message: { type: String, reflect: true },
     
     };
+
+   
+
   }
 }
-
 
 globalThis.customElements.define(BannerApp.tag, BannerApp);
